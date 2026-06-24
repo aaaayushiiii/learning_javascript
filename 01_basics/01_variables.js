2997021c592bc37202a4de18791ad077e98d6b7b
Aayushi Chouksey <aayushichouksey1507@gmail.com>
2026-06-24 22:45:53 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const id = 12345
+let name = "aayushi"
+var i = '1' // is not used because of scope block problem
+city = "mumbai"// can be done but it is bad practice
+
+//const id = 123456// not allowed 
+name = "aayushi chouksey"
+i = '0'
+city = "bengluru"
+let age// treated undefined by javascript
+
+/*  
+never use var 
+*/
+console.log([id,name,i,city,age])
