a48f0d3d1c7982ace39ee5d80b3527834e8cc3b8
Aayushi Chouksey <aayushichouksey1507@gmail.com>
2026-06-25 00:38:02 +0000

    dataTypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,12 @@
+"use strict"
+
+let name = "aayushi"
+let age = 19
+let isit = true
+let num = null
+let ask;
+
+// null is  satndalone value 
+
+console.log(typeof undefined)
+console.log(typeof null)
